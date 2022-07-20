8d4a6dc060e0ac683dc2cd3460698d53bb7b95dc
shera-s <sheraofficials@gmail.com>
2022-07-20 21:19:09 +0530

    commit by shera

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-import { UserReducer } from "./Reducers/userreducer";
+import { RegisterReducer, UserReducer } from "./Reducers/userreducer";
 
 const reducer = combineReducers({
-  User:UserReducer
+  User:UserReducer,
+  RegisterUser:RegisterReducer
 });
 
 let initialState = {
